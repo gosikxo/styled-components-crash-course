39bf68b9913a03a61cc4a7e396cf6ce933b75483
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-05-12 13:20:48 +0100

    Add global styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { ThemeProvider } from 'styled-components';
 import { Header } from './components/Header.js';
 import { Container } from './components/styles/Container.style.js';
+import GlobalStyles from './components/styles/Global.js';
 
 const theme = {
   colors: {
     header: '#ebfbff',
-    body: 'fff',
+    body: '#fff',
     footer: '#003333'
   }
 }
@@ -14,6 +15,7 @@ function App () {
   return (
     <ThemeProvider theme={theme}>
       <>
+        <GlobalStyles />
         <Header />
         <Container>
           <h1>Hi</h1>
